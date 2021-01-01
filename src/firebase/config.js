d2b1d1992c08149d09bba9be1b09c6714893074a
Walid Jamarin <walidjamarin21@gmail.com>
2021-01-01 21:50:37 +0800

    setup firebase connection

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoEsZhP1CPLt8pX4IyyggeRM4qUyoHkS8",
+  authDomain: "muso-ninjas-a1552.firebaseapp.com",
+  projectId: "muso-ninjas-a1552",
+  storageBucket: "muso-ninjas-a1552.appspot.com",
+  messagingSenderId: "1025851385318",
+  appId: "1:1025851385318:web:a00b84653e54cb0a77a227"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectAuth, projectFirestore, timestamp }
