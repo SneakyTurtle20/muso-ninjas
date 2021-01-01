059d14b733415b3ffa9f8a0748f2b83a8d0a5d31
Walid Jamarin <walidjamarin21@gmail.com>
2021-01-01 22:20:23 +0800

    add pending state to requests

--- src/composables/useCollection.js
+++ src/composables/useCollection.js
@@ -4,21 +4,23 @@ import { projectFirestore } from '../firebase/config'
 const useCollection = (collection) => {
 
   const error = ref(null)
-
+  const isPending = ref(false)
   // add a new document
   const addDoc = async (doc) => {
     error.value = null
-
+    isPending.value = true
     try {
+      isPending.value = false
       await projectFirestore.collection(collection).add(doc)
     }
     catch(err) {
       console.log(err.message)
+      isPending.value = false
       error.value = 'could not send the message'
     }
   }
 
-  return { error, addDoc }
+  return { error, addDoc, isPending }
 
 }
 
